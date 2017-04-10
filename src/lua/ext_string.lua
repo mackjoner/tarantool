db7d8a89595df59d8e6558c4add5686ef3b6798b
bigbes <bigbes@gmail.com>
2017-04-10 17:23:23 +0300

    Adding string.ljust(), string.rjust() and string.center()
    * `string.ljust(inp, width[, char])`  - returns left-justified string
      filled with charachter 'char' (' ' by default).
    * `string.rjust(inp, width[, char])`  - returns right-justified string
      filled with charachter 'char' (' ' by default)
    * `string.center(inp, width[, char])` - returns centered string
      filled with charachter 'char' (' ' by default)
    
    closes gh-2214

--- src/lua/ext_string.lua
+++ src/lua/ext_string.lua
@@ -89,6 +89,87 @@ local function string_split(inp, sep, max)
     return string_basesplit(inp, sep, max):totable()
 end
 
+--- Left-justify string in a field of given width.
+-- Append "width - len(inp)" chars to given string. Input is never trucated.
+-- @function ljust
+-- @string       inp    the string
+-- @int          width  at least bytes to be returned
+-- @string[opt]  char   char of length 1 to fill with (" " by default)
+-- @returns             result string
+local function string_ljust(inp, width, char)
+    if type(inp) ~= 'string' then
+        error("string.ljust argument #1 should be a string", 2)
+    end
+    if type(width) ~= 'number' or width < 0 then
+        error("string.ljust argument #2 should be a natural number", 2)
+    end
+    if char ~= nil and (type(char) ~= 'string' or #char ~= 1) then
+        error("string.ljust argument #3 should be a char (length 1) or nil", 2)
+    end
+    char = char or " "
+    local delta = width - #inp
+    if delta < 0 then
+        return inp
+    end
+    return inp .. char:rep(delta)
+end
+
+--- Right-justify string in a field of given width.
+-- Prepend "width - len(inp)" chars to given string. Input is never trucated.
+-- @function rjust
+-- @string       inp    the string
+-- @int          width  at least bytes to be returned
+-- @string[opt]  char   char of length 1 to fill with (" " by default)
+-- @returns             result string
+local function string_rjust(inp, width, char)
+    if type(inp) ~= 'string' then
+        error("string.rjust argument #1 should be a string", 2)
+    end
+    if type(width) ~= 'number' or width < 0 then
+        error("string.rjust argument #2 should be a natural number", 2)
+    end
+    if char ~= nil and (type(char) ~= 'string' or #char ~= 1) then
+        error("string.rjust argument #3 should be a char (length 1) or nil", 2)
+    end
+    char = char or " "
+    local delta = width - #inp
+    if delta < 0 then
+        return inp
+    end
+    return char:rep(delta) .. inp
+end
+
+--- Center string in a field of given width.
+-- Prepend and append "(width - len(inp))/2" chars to given string.
+-- Input is never trucated.
+-- @function rjust
+-- @string       inp    the string
+-- @int          width  at least bytes to be returned
+-- @string[opt]  char   char of length 1 to fill with (" " by default)
+-- @returns             result string
+local function string_center(inp, width, char)
+    if type(inp) ~= 'string' then
+        error("string.center argument #1 should be a string", 2)
+    end
+    if type(width) ~= 'number' or width < 0 then
+        error("string.center argument #2 should be a natural number", 2)
+    end
+    if char ~= nil and (type(char) ~= 'string' or #char ~= 1) then
+        error("string.center argument #3 should be a char (length 1) or nil", 2)
+    end
+    char = char or " "
+    local delta = width - #inp
+    if delta < 0 then
+        return inp
+    end
+    local pad_left = math.floor(delta / 2)
+    local pad_right = delta - pad_left
+    return char:rep(pad_left) .. inp .. char:rep(pad_right)
+end
+
 -- It'll automatically set string methods, too.
 string.gsplit = string_gsplit
 string.split  = string_split
+string.ljust  = string_ljust
+string.rjust  = string_rjust
+string.center = string_center
--- test/app-tap/string.test.lua
+++ test/app-tap/string.test.lua
@@ -2,48 +2,78 @@
 
 local tap = require('tap')
 local test = tap.test("string extensions")
-test:plan(16)
+test:plan(2)
 
-test:is_deeply((""):split(""), {""},   "empty split")
-test:is_deeply((""):split("z"), {""},  "empty split")
-test:is_deeply(("a"):split(""), {"a"}, "empty split")
-test:is_deeply(("a"):split("a"), {"", ""}, "split self")
-test:is_deeply(
-    (" 1 2  3  "):split(),
-    {"1", "2", "3"},
-    "complex split on empty separator"
-)
-test:is_deeply(
-    (" 1 2  3  "):split(" "),
-    {"", "1", "2", "", "3", "", ""},
-    "complex split on space separator"
-)
-test:is_deeply(
-    (" 1 2  \n\n\n\r\t\n3  "):split("%s+"),
-    {"", "1", "2", "3", ""},
-    "complex split on non-empty regexp separator"
-) -- also used for regexp splitting testing
-test:is_deeply(
-    ("a*bb*c*ddd"):split("*"),
-    {"a", "bb", "c", "ddd"},
-    "another * separator"
-)
-test:is_deeply(
-    ("dog:fred:bonzo:alice"):split(":", 3),
-    {"dog", "fred", "bonzo:alice"},
-    "testing max separator"
-)
-test:is_deeply(
-    ("///"):split("/"),
-    {"", "", "", ""},
-    "testing splitting on one char"
-)
+-- gh-2211 - string.split() Lua API
+test:test("split/gsplit tests", function(test)
+    test:plan(16)
+    test:is_deeply((""):split(""), {""},   "empty split")
+    test:is_deeply((""):split("z"), {""},  "empty split")
+    test:is_deeply(("a"):split(""), {"a"}, "empty split")
+    test:is_deeply(("a"):split("a"), {"", ""}, "split self")
+    test:is_deeply(
+        (" 1 2  3  "):split(),
+        {"1", "2", "3"},
+        "complex split on empty separator"
+    )
+    test:is_deeply(
+        (" 1 2  3  "):split(" "),
+        {"", "1", "2", "", "3", "", ""},
+        "complex split on space separator"
+    )
+    test:is_deeply(
+        (" 1 2  \n\n\n\r\t\n3  "):split("%s+"),
+        {"", "1", "2", "3", ""},
+        "complex split on non-empty regexp separator"
+    ) -- also used for regexp splitting testing
+    test:is_deeply(
+        ("a*bb*c*ddd"):split("*"),
+        {"a", "bb", "c", "ddd"},
+        "another * separator"
+    )
+    test:is_deeply(
+        ("dog:fred:bonzo:alice"):split(":", 3),
+        {"dog", "fred", "bonzo:alice"},
+        "testing max separator"
+    )
+    test:is_deeply(
+        ("///"):split("/"),
+        {"", "", "", ""},
+        "testing splitting on one char"
+    )
 
--- Testing iterator version
-local result = {"dog", "fred", "bonzo:alice"}
-for pos, val in ("dog:fred:bonzo:alice"):gsplit(":", 3) do
-    test:is(val, result[pos], "checking position " .. pos .. " using for")
-end
-("dog:fred:bonzo:alice"):gsplit(":", 3):enumerate():each(function(pos, val)
-    test:is(val, result[pos], "checking position " .. pos .. " using fun")
+    -- Testing iterator version
+    local result = {"dog", "fred", "bonzo:alice"}
+    for pos, val in ("dog:fred:bonzo:alice"):gsplit(":", 3) do
+        test:is(val, result[pos], "checking position " .. pos .. " using for")
+    end
+    ("dog:fred:bonzo:alice"):gsplit(":", 3):enumerate():each(function(pos, val)
+        test:is(val, result[pos], "checking position " .. pos .. " using fun")
+    end)
 end)
+
+-- gh-2214 - string.ljust()/string.rjust() Lua API
+test:test("ljust/rjust/center", function(test)
+    test:plan(15)
+    test:is(("help"):ljust(0),  "help", "ljust, length 0, do nothing")
+    test:is(("help"):rjust(0),  "help", "rjust, length 0, do nothing")
+    test:is(("help"):center(0), "help", "center, length 0, do nothing")
+
+    test:is(("help"):ljust(3),  "help", "ljust, length 3, do nothing")
+    test:is(("help"):rjust(3),  "help", "rjust, length 3, do nothing")
+    test:is(("help"):center(3), "help", "center, length 3, do nothing")
+
+    test:is(("help"):ljust(5),  "help ", "ljust, length 5, one extra charachter")
+    test:is(("help"):rjust(5),  " help", "rjust, length 5, one extra charachter")
+    test:is(("help"):center(5), "help ", "center, length 5, one extra charachter")
+
+    test:is(("help"):ljust(6),  "help  ", "ljust, length 6, two extra charachters")
+    test:is(("help"):rjust(6),  "  help", "rjust, length 6, two extra charachters")
+    test:is(("help"):center(6), " help ", "center, length 6, two extra charachters")
+
+    test:is(("help"):ljust(6, '.'),  "help..", "ljust, length 6, two extra charachters, custom fill char")
+    test:is(("help"):rjust(6, '.'),  "..help", "rjust, length 6, two extra charachters, custom fill char")
+    test:is(("help"):center(6, '.'), ".help.", "center, length 6, two extra charachters, custom fill char")
+end)
+
+os.exit(test:check() == true and 0 or -1)
