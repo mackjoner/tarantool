c52885df445c05311a85b50ecb7581cc070da262
Roman Tsisyk <roman@tarantool.org>
2017-04-13 09:34:33 +0300

    Move box.info.server and box.info.cluster to upper level
    DON'T MERGE.
    
    Get rid of "cluster" and "server" names in box.info:
    
    ```
    tarantool> box.info
    ---
    - version: 1.7.3-538-g7ee75dee4
      id: 1
      ro: false
      vclock: {}
      uptime: 3
      lsn: 0
      vinyl: []
      pid: 20714
      status: running
      uuid: e6d913b9-a8b8-4873-ba94-14cf4357fec6
      signature: 0
      replication:
        1:
          id: 1
          uuid: e6d913b9-a8b8-4873-ba94-14cf4357fec6
          lsn: 0
      replicaset: 65bb1bf1-f1be-4c5d-adc7-19359336072f
    ```
    
    See #723
    
    TODO: update test-run and all tests

--- test/box/info.test.lua
+++ test/box/info.test.lua
@@ -5,12 +5,16 @@ box.info[23]
 box.info['unknown_variable']
 string.match(box.info.version, '^[1-9]') ~= nil
 string.match(box.info.pid, '^[1-9][0-9]*$') ~= nil
-#box.info.server > 0
+box.info.id > 0
+box.info.uuid == box.space._cluster:get(box.info.id)[2]
+box.info.lsn >= 0
+box.info.signature >= 0
+box.info.ro == false
 box.info.replication[1].id
 box.info.status
 string.len(box.info.uptime) > 0
 string.match(box.info.uptime, '^[1-9][0-9]*$') ~= nil
-box.info.cluster.uuid == box.space._schema:get{'cluster'}[2]
+box.info.replicaset == box.space._schema:get{'cluster'}[2]
 t = {}
 for k, _ in pairs(box.info()) do table.insert(t, k) end
 table.sort(t)
